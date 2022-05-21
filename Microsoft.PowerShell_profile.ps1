d75adee6f81ccaae8921bc410afe8bab4a9edd4e
Elmegaard <elmegaard39@gmail.com>
2022-05-21 14:55:25 +0200

    Added psreadline config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,10 +5,18 @@ if ($host.Name -eq 'ConsoleHost')
 {
     Import-Module PSReadLine
 }
-Import-Module -Name Terminal-Icons
 
+Import-Module -Name Terminal-Icons
 oh-my-posh init pwsh --config E:\Git\Oh-My-Posh-Theme\elmegaard.omp.json | Invoke-Expression
 
+# Setup PSReadline
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete # Make tab completion work like bash
+Set-PSReadlineOption -BellStyle None # Disable annoying beep on error
+
+# Custom aliases
+Set-Alias ex explorer.exe
+
+# Custom functions
 function New-Link ($from, $to) {
     New-Item -Path $to -ItemType SymbolicLink -Value $from
 }
